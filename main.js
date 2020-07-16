6d9c63532ae274e3be861336eb94190559ef3c14
yoonvelop <yoonvelop@gmail.com>
2020-07-16 15:57:59 +0900

    scroll to section

--- main.js
+++ main.js
@@ -3,10 +3,31 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height; //실제로 사용자에게 보여지는 사이즈를 가져오는 함수
 document.addEventListener("scroll", () => {
-  console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
     navbar.classList.remove("navbar--trans");
   } else {
     navbar.classList.add("navbar--trans");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
